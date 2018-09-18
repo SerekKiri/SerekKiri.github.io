67b6eb8ceba6ed65c15509a891a0ffbe3a5a3f4e
SerekKiri <kacper.potyrala@gmail.com>
2018-09-18 22:00:02 +0200

    Update

--- js/modal.js
+++
@@ -1,9 +0,0 @@
-function toggleModal (element) {
-  if (typeof element === 'string') document.querySelector(element).classList.toggle('is-active')
-  else element.classList.toggle('is-active')
-}
-
-document.querySelectorAll('.modal').forEach(modal => {
-  modal.querySelector('.modal-background').addEventListener('click', ev => toggleModal(modal))
-  modal.querySelector('.modal-close').addEventListener('click', ev => toggleModal(modal))
-})
